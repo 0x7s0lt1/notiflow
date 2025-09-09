28ec01370d1a245c39797bdf2b4687744adf5b86
0x7s0lt1 <zsoltfehervari@gmx.com>
2025-09-09 04:41:06 +0200

    wip

--- app/create-alert.tsx
+++ app/create-alert.tsx
@@ -1,11 +1,51 @@
 import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
-import {Text} from "react-native";
+import {Platform, Text} from "react-native";
+import {useEffect, useRef, useState} from "react";
+import { InstalledApps } from 'react-native-launcher-kit';
 
 const CreateAlert = () => {
+
+    let appsFetched = useRef<boolean>(false);
+    const [apps, setApps] = useState<any|null>(null);
+
+    useEffect(() => {
+
+        if (!appsFetched.current) {
+            appsFetched.current = true;
+            (async () => {
+
+                if(Platform.OS !== "web"){
+                    const Apps = InstalledApps.getApps();
+                    console.log(Apps);
+                    setApps(Apps);
+                }
+
+            })();
+        }
+
+    }, []);
+
     return (
         <SafeAreaProvider>
             <SafeAreaView className={"flex flex-col items-center justify-center w-full h-full p-4"}>
                 <Text className={"text-2xl font-bold text-center"}> Create Alert </Text>
+
+                {/*{ apps &&*/}
+                {/*    <FlatList*/}
+                {/*        className={"mt-4 p-2 w-full"}*/}
+                {/*        data={apps}*/}
+                {/*        renderItem={ (i: any) =>  {*/}
+                {/*            return (*/}
+                {/*                <View className={"flex bg-transparent my-2 flex-row items-center justify-between shadow w-full rounded-2xl p-4"}>*/}
+                {/*                    <Text className={"bg-transparent text-2xl font-bold"}>{i.item.label}</Text>*/}
+                {/*                    <Text className={"bg-transparent text-2xl font-bold"}>{i.item.packageName}</Text>*/}
+                {/*                </View>*/}
+                {/*            )*/}
+                {/*        }}*/}
+                {/*        keyExtractor={item => item.id}*/}
+                {/*    />*/}
+                {/*}*/}
+
             </SafeAreaView>
         </SafeAreaProvider>
     )
--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,5 @@
-import {Text, View, FlatList, Platform, ScrollView} from "react-native";
-import {Image} from "expo-image";
+import {Text, View, FlatList, Platform, ScrollView, Alert} from "react-native";
 import {useEffect, useRef, useState} from "react";
-import { InstalledApps } from 'react-native-launcher-kit';
 import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
 import {Link} from "expo-router";
 import useAlertStorage from "@/hooks/use-alert-storage";
@@ -11,8 +9,22 @@ export default function Index() {
 
     let storageFetched = useRef<boolean>(false);
 
-    const { storage, fetchStorage, removeAlert, addAlert } = useAlertStorage();
-    // const [apps, setApps] = useState<any|null>(null);
+    const { storage, fetchStorage, removeAlert } = useAlertStorage();
+
+    const handleRemovePress = async (id: string) => {
+
+        Alert.alert("Remove Alert", "Are you sure you want to remove this alert?",
+            [
+                {
+                    text: "Cancel"
+                },
+                {
+                    text: "Remove",
+                    onPress: async () => await removeAlert(id)
+                }
+            ]
+        )
+    }
 
     useEffect(() => {
 
@@ -22,12 +34,6 @@ export default function Index() {
             (async ()=>{
 
                 try{
-
-                    // if(Platform.OS === "android"){
-                    //     const Apps = InstalledApps.getApps();
-                    //     console.log(Apps);
-                    //     setApps(Apps);
-                    // }
 
                    await fetchStorage();
 
@@ -55,12 +61,12 @@ export default function Index() {
                       data={storage}
                       renderItem={ (i: any) =>  {
                           return (
-                              <View className={"grid grid-cols-4 items-center shadow rounded-md p-4 my-2"}>
-                                  <Link href={`/alert/${i.item.id}`} className={"text-2xl font-bold col-span-3"} >
+                              <View className={"grid grid-cols-4 items-center border border-muted shadow rounded-md p-4 my-2"}>
+                                  <Link href={`/alert/${i.item.id}`} className={"col-span-3 text-2xl font-bold"} >
                                       {i.item.targetPackage.label}
                                   </Link>
                                   <View className={"col-span-1 flex flex-col justify-end bg-red-300 p-2 rounded-full items-center"}>
-                                      <Trash className={""} onPress={async () => await removeAlert(i.item.id)} />
+                                      <Trash className={""} onPress={async () => handleRemovePress(i.item.id)} />
                                   </View>
                               </View>
                           )
@@ -70,21 +76,6 @@ export default function Index() {
 
               }
 
-              {/*{ apps &&*/}
-              {/*    <FlatList*/}
-              {/*        className={"mt-4 p-2 w-full"}*/}
-              {/*        data={apps}*/}
-              {/*        renderItem={ (i: any) =>  {*/}
-              {/*            return (*/}
-              {/*                <View className={"flex bg-transparent my-2 flex-row items-center justify-between shadow w-full rounded-2xl p-4"}>*/}
-              {/*                    <Text className={"bg-transparent text-2xl font-bold"}>{i.item.label}</Text>*/}
-              {/*                    <Text className={"bg-transparent text-2xl font-bold"}>{i.item.packageName}</Text>*/}
-              {/*                </View>*/}
-              {/*            )*/}
-              {/*        }}*/}
-              {/*        keyExtractor={item => item.id}*/}
-              {/*    />*/}
-              {/*}*/}
 
               <Link href="/create-alert" className={"bg-black text-white border border-muted border-dashed absolute bottom-16 right-4 z-50 rounded-full p-4 shadow-2xl"}>
                   <Plus size={50} color="white" className={"text-white"} />
